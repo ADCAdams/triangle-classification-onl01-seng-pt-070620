5e27d24994af232250a19d597c569892d449c886
Andy Adams <andrewdcadams@gmail.com>
2020-08-16 17:05:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   # write code here
   
-  attr_accessor ::equilateral
+  attr_accessor ::equilateral, :isosceles
   
   class TriangleError < StandardError
     # triangle error code
