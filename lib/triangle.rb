a7b70ac191fd9c6dbef4d17b8e6be3226f825f32
Andy Adams <andrewdcadams@gmail.com>
2020-08-16 17:26:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
       begin
         raise TriangleError
       rescue TriangleError => error
-      puts ((@equilateral+@isosceles) >= @scalene || (@equilateral+@scalene) >= @isosceles || (@isosceles+@scalene) >= @equilateral)
+      puts ((@equilateral+@isosceles) (@equilateral+@scalene) (@isosceles+@scalene) 
       puts "two sides big"
           puts error.message
       end
