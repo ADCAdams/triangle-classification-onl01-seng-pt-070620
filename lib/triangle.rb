b72e18763a80c22ff2c4466775bf6039a3407d23
Andy Adams <andrewdcadams@gmail.com>
2020-08-16 17:14:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,13 +18,15 @@ class Triangle
       rescue PartnerError => error
           puts error.message
       end
-    elsif (@equilateral+@isosceles) < @scalene || (@equilateral+@scalene) < @isosceles || (@isosceles+@scalene) < @equilateral
+    elsif (@equilateral+@isosceles) >= @scalene || (@equilateral+@scalene) >= @isosceles || (@isosceles+@scalene) >= @equilateral
       begin
         raise PartnerError
       rescue PartnerError => error
           puts error.message
       end
-    else
+    elsif @equilateral == @isosceles == @scalene
+      :equilateral
+    elsif 
       
     end   #end if
     
